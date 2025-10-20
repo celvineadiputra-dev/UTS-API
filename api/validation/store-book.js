b189be39c80290ad06128fdfa072cb8c2b9a4ff3
celvine <celvineadiputra2@gmail.com>
2025-10-20 13:54:50 +0700

    Change : [Change int to string validation schema]

--- api/validation/store-book.js
+++ api/validation/store-book.js
@@ -3,7 +3,7 @@ import z from "zod";
 const StoreBook = z.object({
 	title: z.string("Title is required"),
 	author: z.string("Author is required"),
-	published_year: z.number("Published Year is Number").int("Published Year is required"),
+	published_year: z.string("Published Year is required"),
 });
 
 export default StoreBook
