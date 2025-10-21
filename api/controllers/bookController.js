a55c7e9f9a6cc36a120dcd9771d4b8d724578ee9
celvine <celvineadiputra2@gmail.com>
2025-10-21 08:08:57 +0700

    Enhancement : [Order by]

--- api/controllers/bookController.js
+++ api/controllers/bookController.js
@@ -1,3 +1,4 @@
+import { desc } from "drizzle-orm";
 import z from "zod";
 import db from "../database/database.js";
 import { booksTable } from "../database/db/schema.js";
@@ -5,38 +6,38 @@ import ApiResponse from "../utils/api-response.js";
 import StoreBook from "../validation/store-book.js";
 
 export const books = async (_, reply) => {
-	const books = await db.select().from(booksTable);
+	const books = await db.select().from(booksTable).orderBy(desc(booksTable.id));
 	reply.code(200).send(ApiResponse(200, "Books", books));
 };
 
 export const storeBook = async (request, reply) => {
 	try {
-        const {
-            title,
-            author,
-            published_year
-        } = request.body
+		const { title, author, published_year } = request.body;
 
-        const form = StoreBook.safeParse({
-            title,
-            author,
-            published_year
-        })
+		const form = StoreBook.safeParse({
+			title,
+			author,
+			published_year,
+		});
 
-        if(!form.success) {
-            return reply.code(402).send(
-                ApiResponse(402, "Validation error", z.flattenError(form.error)?.fieldErrors)
-            )
-        }
-        
-        const newBook = await db.insert(booksTable).values(form.data).returning()
-        
+		if (!form.success) {
+			return reply
+				.code(402)
+				.send(
+					ApiResponse(
+						402,
+						"Validation error",
+						z.flattenError(form.error)?.fieldErrors,
+					),
+				);
+		}
+
+		const newBook = await db.insert(booksTable).values(form.data).returning();
+
 		reply.code(201).send(ApiResponse(201, "Success store new book", newBook));
 	} catch (error) {
-        console.log(error);
-        
-		reply
-			.code(500)
-			.send(ApiResponse(500, error.message));
+		console.log(error);
+
+		reply.code(500).send(ApiResponse(500, error.message));
 	}
 };
