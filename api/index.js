d7274fd3ee09cdb617247c172302fff169efef15
celvine <celvineadiputra2@gmail.com>
2025-10-21 08:03:13 +0700

    Add : [prefix]

--- api/index.js
+++ api/index.js
@@ -14,8 +14,8 @@ app.get("/", async (_, reply) => {
 	reply.code(200).send(ApiResponse(200, "Selamat datang di UTS IF31"));
 });
 
-app.get("/books", books);
-app.post("/books", storeBook);
+app.get("/api/books", books);
+app.post("/api/books", storeBook);
 
 export default async function handler(req, reply) {
 	await app.ready();
